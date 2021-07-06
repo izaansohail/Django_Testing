c2a6b1b3ebe76c0c09b7c7ee171c118758124231
Izaan Sohail <intern.it-izaan@crescent.com.pk>
2021-07-06 17:12:46 +0500

    Model Designed

--- home/admin.py
+++ home/admin.py
@@ -3,3 +3,8 @@ from home.models import Contact
 
 # Register your models here.
 admin.site.register(Contact)
+class ContactView(admin.ModelAdmin):
+    list_display = ['email','password',
+    'name','address','organization',
+    'contact_number','checkin','checkout',
+    'contact_person','purpose','img_location']
--- home/models.py
+++ home/models.py
@@ -4,14 +4,14 @@ from django.db import models
 class Contact(models.Model):
     email = models.CharField(max_length=200,default=None)
     password = models.CharField(max_length=200,default=None)
-    name = models.CharField(max_length=200,default=None)
-    address1 = models.CharField(max_length=200,default=None)
-    organization = models.CharField(max_length=200,default=None)
-    contact_number = models.CharField(max_length=200,default=None)
-    checkin = models.CharField(max_length=200,default=None)
-    checkout = models.CharField(max_length=200,default=None)
-    contact_person = models.CharField(max_length=200,default=None)
-    purpose = models.CharField(max_length=200,default=None)
-
+    name = models.CharField(max_length=200,default=None,null=True)
+    address = models.CharField(max_length=200,default=None,null=True)
+    organization = models.CharField(max_length=200,default=None,null=True)
+    contact_number = models.CharField(max_length=200,default=None,null=True)
+    checkin = models.CharField(max_length=200,default=None,null=True)
+    checkout = models.CharField(max_length=200,default=None,null=True)
+    contact_person = models.CharField(max_length=200,default=None,null=True)
+    purpose = models.CharField(max_length=200,default=None,null=True)
+    img_location = models.CharField(max_length=200,default=None,null=True)
     def __str__(self):
         return self.email
