40b2a4acb4025573c5b9024780c1479ec9036099
IzaanSohail10 <izaansohail10.is@gmail.com>
2021-07-06 13:52:17 +0500

    Initial

---
+++ home/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from home.models import Contact
+from django.contrib import messages
+
+# Create your views here.
+
+def index(request):
+    context  = {
+        'variable1' : "this is sent",
+        'variable2' : "My name is Izaan"
+    }
+    return render(request, 'index.html',context)
+    # return HttpResponse("this is homepage")
+
+def about(request):
+    return render(request, 'about.html')
+    # return HttpResponse("this is about page")
+
+def services(request):
+     return render(request, 'services.html')
+    # return HttpResponse("this is services page")
+
+def contact(request):
+    if request.method == "POST":
+        email = request.POST.get('Email')
+        password = request.POST.get('Password')
+        contact = Contact(email=email , password=password)
+        contact.save()
+        messages.success(request, 'Your Details have been saved in database.')
+    return render(request, 'contact.html')
+    # return HttpResponse("this is contact page")
+
+
