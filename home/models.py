75f6a5cf18ef3ac036287d00c6947a7e19d89e34
Izaan Sohail <intern.it-izaan@crescent.com.pk>
2021-07-07 16:12:55 +0500

    Data saving on admin panel

--- home/models.py
+++ home/models.py
@@ -2,8 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Contact(models.Model):
-    email = models.CharField(max_length=200,default=None)
-    password = models.CharField(max_length=200,default=None)
     name = models.CharField(max_length=200,default=None,null=True)
     address = models.CharField(max_length=200,default=None,null=True)
     organization = models.CharField(max_length=200,default=None,null=True)
@@ -14,4 +12,4 @@ class Contact(models.Model):
     purpose = models.CharField(max_length=200,default=None,null=True)
     img_location = models.CharField(max_length=200,default=None,null=True)
     def __str__(self):
-        return self.email
+        return self.name
--- home/views.py
+++ home/views.py
@@ -19,9 +19,18 @@ def services(request):
 
 def contact(request):
     if request.method == "POST":
-        email = request.POST.get('Email')
-        password = request.POST.get('Password')
-        contact = Contact(email=email , password=password)
+        name = request.POST.get('name')
+        address = request.POST.get('address')
+        organization = request.POST.get('organization')
+        contact_number = request.POST.get('contact_number')
+        checkin = request.POST.get('checkin')
+        checkout = request.POST.get('checkout')
+        contact_person = request.POST.get('contact_person')
+        purpose = request.POST.get('purpose')
+        img_location = request.POST.get('img_location')
+        contact = Contact(name=name,address=address,
+        organization=organization,contact_number=contact_number,checkin=checkin,checkout=checkout,
+        contact_person=contact_person,purpose=purpose,img_location=img_location)
         contact.save()
         messages.success(request, 'Your Details have been saved in database.')
     return render(request, 'contact.html')
